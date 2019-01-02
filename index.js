ed1c37a8688912a8a2c6ceaeea0f93fbd9142167
jaklahaulps <liu.perry@gmail.com>
2019-01-01 20:24:21 -0500

    done

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let counter = 0;
+
+  function elementValue(element){
+    if (Array.isArray(element)){
+      element.forEach((newElement)=>{
+        elementValue(newElement);
+      })
+    } else {
+      return counter += element;
+    }
+  }
+
+  while(array.length > 0){
+    let checkVal = array.shift();
+    elementValue(checkVal);
+  }
+
+  return counter;
 }
 
 module.exports = sumItems;
